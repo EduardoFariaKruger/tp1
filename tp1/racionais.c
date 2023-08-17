c6cbc60af9261cf96c9116a1eda23f35974ce3cb
EDUARDO FARIA KRUGER <efk23@inf.ufpr.br>
2023-08-17 17:08:54 -0300

    finalizado o trabalho

--- tp1/racionais.c
+++ tp1/racionais.c
@@ -11,11 +11,18 @@
  * aqui.
 */
 
+
+/*
+ * Essa função gera um número aleatório dentro do intervalo [min..max].
+*/
 int aleat(int min, int max)
 {
-    srand(time(NULL));
     return (min + rand() % (max - min + 1));
 }
+
+/*
+ * Essa função calcula o MDC de dois números a e b usando o método de euclides.
+*/
 int mdc(int a, int b)
 {
     while (b != 0)
@@ -26,61 +33,123 @@ int mdc(int a, int b)
     }
     return a;
 }
+
+/*
+ * Essa função usa do método do produto pelo quociente para calcular o MMC entre a e b usando do MDS entre a e b.\
+*/
 int mmc(int a, int b)
 {
     return a * (b / mdc(a, b));
 }
+
+/*
+ * Nessa função, a simplificação é feita dividindo tanto o numerador quando o denominador pelo MDC entre eles.
+*/
 struct racional simplifica_r(struct racional r)
 {
-    int val = 2;
-    while ((val < r.num) && (val < r.den))
-    {
-        if (((r.num % val) == 0) && ((r.den % val) == 0))
-        {
-            r.num = r.num / val;
-            r.den = r.den / val;
-        }
-        val++;
-    }
+    int maiorDivisor = mdc(r.num, r.den);
+    r.num = r.num / maiorDivisor;
+    r.den = r.den / maiorDivisor;
     return r;
 }
+
+/*
+ * Essa função gera dois valores aleatórios no intervalo [0..n] para serem o numerador e o denominador de um número fracionário.
+*/
 struct racional sorteia_r(int n)
 {
-    struct racional teste;
-    teste.num = 2;
-    teste.den = 2;
-    return teste;
+    struct racional r;
+    r.num = aleat(0, n);
+    r.den = aleat(0, n);
+    if (r.den == 0)
+    {
+        r.valido = 0;
+        return r;
+    }
+    return simplifica_r(r);
 }
+
+/*
+ * Essa função recebe 2 valores "numerador" e "denominador" e os coloca em uma struct de número racional.
+*/
 struct racional cria_r(int numerador, int denominador)
 {
-    struct racional teste;
-    teste.num = 2;
-    teste.den = 2;
-    return teste;
+    struct racional r;
+    r.num = numerador;
+    r.den = denominador;
+    if (r.den == 0)
+    {
+        r.valido = 0;
+        return r;
+    }
+    return simplifica_r(r);
 }
+
+/*
+ * Essa função usa dos valores de num e den de uma struct para imprimir um número racional no formato "num/den".
+*/
 void imprime_r(struct racional r)
 {
-    printf("%d / %d", r.num, r.den);
+    if ((r.num == 0) || (r.den == 1))
+    {
+        printf("%d  ", r.num);
+    }
+    else
+    {
+        printf("%d/%d  ", r.num, r.den);
+    }
 }
+
+/*
+ * Essa função retorna 1 se o denominador for diferente de zero e 0 se o denominador for igual a zero.
+*/
 int valido_r(struct racional r)
 {
     return (r.den != 0);
 }
+
+/*
+ * Essa função realiza a soma de frações usando o conceito de MMC e retorna o resultado simplificado
+*/
 struct racional soma_r(struct racional r1, struct racional r2)
 {
+    int minMC = mmc(r1.den, r2.den);
+    r1.num = minMC/r1.den*r1.num + minMC/r2.den*r2.num;
+    r1.den = minMC;
+    simplifica_r(r1);
     return r1;
 }
+struct racional subtrai_r(struct racional r1, struct racional r2)
+{
+    int minMC = mmc(r1.den, r2.den);
+    r1.num = minMC/r1.den*r1.num - minMC/r2.den*r2.num;
+    r1.den = minMC;
+    simplifica_r(r1);
+    return r1;
+}
+
+/*
+ * Essa função realiza a operação de multiplicação entre fraçoes e retorna o resultado na forma simplificada.
+*/
 struct racional multiplica_r(struct racional r1, struct racional r2)
 {
     r1.num = r1.num * r2.num;
     r1.den = r1.den * r2.den;
-    simplifica_r(r1);
+    r1 = simplifica_r(r1);
     return r1;
 }
+
+/*
+ * Essa função realiza a operação usando da definição de divisão de frações, retornando o resultado na forma simplificada.
+*/
 struct racional divide_r(struct racional r1, struct racional r2)
 {
     r1.num = r1.num * r2.den;
     r1.den = r1.den * r2.num;
-    simplifica_r(r1);
-    return r1;
+    if (r1.den == 0)
+    {
+        r1.valido = 0;
+        return r1;
+    }
+    return simplifica_r(r1);
 }
--- tp1/tp1.c
+++ tp1/tp1.c
@@ -3,13 +3,47 @@
 /* coloque seus includes aqui */
 
 #include <stdio.h>
-#include "racionais.c"
+#include "racionais.h"
+#include <stdlib.h>
+
+
+int n, max, i;
+struct racional r1, r2, soma, subtracao, multiplicacao, divisao;
 
 int main (){
 
-    /* coloque seu codido aqui */
+    srand(0);
+    scanf("%d", &n);
+    scanf("%d", &max);
+    
 
-    printf("asdasdasd\n");
+    for (i = 1 ; i<=n; i++)
+    {
+        printf("%d: \n", i);
+        r1 = sorteia_r(max);
+        r2 = sorteia_r(max);
+        imprime_r(r1);
+        imprime_r(r2);
+        if ((!valido_r(r1)) || (!valido_r(r2)))
+        {
+            printf("NUMERO INVALIDO\n");
+            return 1;
+        }
+        soma = soma_r(r1, r2);
+        subtracao = subtrai_r(r1, r2);
+        multiplicacao = multiplica_r(r1, r2);
+        divisao = divide_r(r1, r2);
+        if (!valido_r(divisao))
+        {
+            printf("NUMERO INVALIDO\n");
+            return 1;
+        }
+        imprime_r(soma);
+        imprime_r(subtracao);
+        imprime_r(multiplicacao);
+        imprime_r(divisao);
+        printf("\n");
+    }
 
     return 0;
 }
